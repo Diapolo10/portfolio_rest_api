fd62d8e798ac2ab7df390106d6f5edfb7ba39b64
Lari Liuhamo <lari.liuhamo@gmail.com>
2024-11-04 10:27:51 +0200

    [PATCH] Ruff fix

--- src/portfolio_rest_api/routers/email.py
+++ src/portfolio_rest_api/routers/email.py
@@ -1,5 +1,7 @@
 """Handle email routes."""
 
+from __future__ import annotations
+
 from fastapi import APIRouter, status
 
 from portfolio_rest_api.models import Email
@@ -11,6 +13,6 @@ router = APIRouter(
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-async def post_send_email(email: Email) -> Email:  # noqa: RUF029
+async def post_send_email(email: Email) -> Email:
     """Handle email requests and forward them to the configured email address."""
     return email  # NOTE: Make this do stuff
--- tests/test_default.py
+++ tests/test_default.py
@@ -1,7 +1,8 @@
 """Stub unit test file."""
 
+from portfolio_rest_api.config import DEFAULT_CONFIG_FILE_PATH
+
 
 def test_stub():
     """Default test."""
-    assert True
-    assert not False
+    assert DEFAULT_CONFIG_FILE_PATH.exists()
